2fd3348635ecdafa5ba7101fe7c4acffc1872dc3
miciella <201910804@feualabang.edu.ph>
2022-08-14 13:19:29 +0800

    edited port

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/registration', function (req, res){
 
 
 
-app.listen(3000, function (){
+app.listen(process.env.PORT || 3000, function (){
     console.log("Server started on port 3000")
 })
 

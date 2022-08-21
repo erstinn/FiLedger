6ba080ac949a656e209d45cf33a6190fde62e9c2
miciella <201910804@feualabang.edu.ph>
2022-08-21 14:27:41 +0800

    added some port

--- server.js
+++ server.js
@@ -56,8 +56,7 @@ app.get('/management/manage-documents/approver',function (req,res){
 })
 
 
-
-app.listen(3000, function (){
+app.listen(process.env.PORT || 3000, function (){
     console.log("Server started on port 3000")
 })
 

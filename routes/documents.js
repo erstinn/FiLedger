1502957c714b2360902ea4cabf8ceae9aff91979
erstinn <erstin.adean@gmail.com>
2022-08-31 14:52:43 +0800

    finishing dashboard

--- routes/documents.js
+++ routes/documents.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res){
-    res.render("documents");
+    res.render("documents", {username : req.body.username});
 })
 
 module.exports = router

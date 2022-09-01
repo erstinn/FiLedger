0a7ce00ad8a4018d43536b1b5856a2330e7dd8e7
Mark <201820002@feualabang.edu.ph>
2022-09-02 02:47:19 +0800

    added buttons and modals/ui revision

--- public/js/view-documents.js
+++ public/js/view-documents.js
@@ -1,60 +1,46 @@
 const users = [
     {
-        name:"Mark",
-        department:"CS",
-        document:1,
-        activity:"downloaded",
+        name: 'Mark',
+        department: 'CS',
+        document: 1,
+        activity: 'downloaded',
     },
     {
-        name:"Dav",
-        department:"CS",
-        document:999,
-        activity:"edited",
+        name: 'Dav',
+        department: 'CS',
+        document: 999,
+        activity: 'edited',
     },
     {
-        name:"Thor",
-        department:"MMA",
-        document:0,
-        activity:"downloaded",
+        name: 'Thor',
+        department: 'MMA',
+        document: 0,
+        activity: 'downloaded',
     },
     {
-        name:"Berbaderg",
-        department:"MMA",
-        document:1200,
-        activity:"downloaded",
+        name: 'Berbaderg',
+        department: 'MMA',
+        document: 1200,
+        activity: 'downloaded',
+    },
+    {
+        name: 'TamTam',
+        department: 'Engineering',
+        document: 10000,
+        activity: 'edited',
+    },
+];
 
-    },
-    {
-        name:"TamTam",
-        department:"Engineering",
-        document:10000,
-        activity:"edited",
-    },
-]
-const userFilter = document.querySelector("#userFilter");
-users.forEach((item)=>{
-    const placeholder = document.createElement("option");
-    placeholder.selected = true;
-    placeholder.disabled = true;
-    placeholder.hidden = true;
-    placeholder.text = "Select user";
-    const newOption = document.createElement("option");
-    newOption.classList.add("userItem");
-    newOption.value = item.name;
-    newOption.text = item.name
-
-    userFilter.appendChild(placeholder)
-    userFilter.appendChild(newOption);
-})
-
-const data = document.querySelectorAll(".data")
-const revertButt = document.querySelector(".revert");
-data.forEach(item=>{
-    item.addEventListener("click",()=>{
-        if(document.querySelector(".clickedData") != null){
-            document.querySelector(".clickedData").classList.remove("clickedData")
+const data = document.querySelectorAll('.data');
+const revertButt = document.querySelector('.revert');
+data.forEach(item => {
+    item.addEventListener('click', () => {
+        if (document.querySelector('.clickedData') != null) {
+            document
+                .querySelector('.clickedData')
+                .classList.remove('clickedData');
         }
-        item.classList.add("clickedData")
+        item.classList.add('clickedData');
         revertButt.disabled = false;
-    })
-})
+    });
+});

4a58295609bd0f44cf46e483783ee9305f2f7e23
Mark <201820002@feualabang.edu.ph>
2022-11-01 11:41:53 +0800

    change db access

--- routes/administration.js
+++ routes/administration.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const nano = require('nano')('http://root:root@127.0.0.1:5984/');
+// const nano = require('nano')('http://root:root@127.0.0.1:5984/');
+const nano = require('nano')('http://administrator:qF3ChYhp@127.0.0.1:5984/');
 const docsDB = nano.db.use('documents');
 
 
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 
 // databases
-// const nano = require('nano')('http://administrator:qF3ChYhp@127.0.0.1:5984/');
+const nano = require('nano')('http://administrator:qF3ChYhp@127.0.0.1:5984/');
 //const nano = require('nano')('http://admin:mysecretpassword@127.0.0.1:5984/');
 // const nano = require('nano')('http://admin:pw123@127.0.0.1:5984/');
 // const nano = require('nano')('http://root:root@127.0.0.1:5984/');

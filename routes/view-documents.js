6a165c59b297af455029021b6bcd33d0545a9e99
erstinn <erstin.adean@gmail.com>
2022-11-09 07:00:59 +0800

    fixed

--- routes/view-documents.js
+++ routes/view-documents.js
@@ -5,30 +5,49 @@ const nano = require('nano')('http://administrator:qF3ChYhp@127.0.0.1:5984/');
 //const nano = require('nano')('http://admin:mysecretpassword@127.0.0.1:5984/');
 // const nano = require('nano')('http://root:root@127.0.0.1:5984/');
 // const nano = require('nano')('http://admin:pw123@127.0.0.1:5984/');
-const docsDB = nano.db.use('documents');
+const docsOrg1DB = nano.db.use('org1-documents');
+const docsOrg2DB = nano.db.use('org2-documents');
+// const docsDB = nano.db.use('org1-documents');
 
 
 
 router.get('/:id', async function (req,res){
-    const data = await  docsDB.find({
-        selector:{
-            "_id":req.params.id
-        }
-    })
-    res.render("view-documents",{data:data,username : req.session.username});
+    if (req.session.org==='org1'){
+        const data = await  docsOrg1DB.find({selector:{"_id":req.params.id}})
+        res.render("view-documents",{data:data,username : req.session.username});
+    } else if (req.session.org==='org2'){
+        const data = await  docsOrg2DB.find({selector:{"_id":req.params.id}})
+        res.render("view-documents",{data:data,username : req.session.username});
+    }
 })
 
 router.post('/delete/:id', async (req,res)=>{
-    const rev = await docsDB.find({selector:{
-        "_id":req.params.id
-    }})
-    await docsDB.destroy(req.params.id,rev.docs[0]._rev)
-    res.redirect('/all-documents')
+    if (req.session.org==='org1'){
+        const rev = await docsOrg1DB.find({selector:{"_id":req.params.id}})
+        await docsOrg1DB.destroy(req.params.id,rev.docs[0]._rev)
+        res.redirect('/all-documents')
+    } else if (req.session.org==='org2') {
+        const rev = await docsOrg2DB.find({selector: {"_id": req.params.id}})
+        await docsOrg2DB.destroy(req.params.id, rev.docs[0]._rev)
+        res.redirect('/all-documents')
+    }
 })
 
 router.post('/downloads/:name',async(req,res)=>{
-    const file = `uploads/${req.params.name}`;
-    res.download(file,req.params.name)
+    console.log('download deputa')
+    // const file = `uploads/${req.params.name}`;
+
+
+    // res.download(file,req.params.name)
 })
+
+//todo ======================================= FUNCTIONS =======================================
+function downloadAttachment (db, org){
+    if (org==='org1'){
+        const body = b.attachment.get('')
+    }
+}
+
+
 
 module.exports = router
